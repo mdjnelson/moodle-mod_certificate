5923d41d2859e7e9d35453c428ea91a136b459b2
huguei <huguei>
2004-07-23 02:23:38 +0000

    First version of the certificate module's language files

---
+++ lang/en/certificate.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$string['modulename'] = 'Certificate';
+$string['modulenameplural'] = 'Certificates';
+
+$string['advertencia'] = 'Only students can download the certificate in this version';
+
+$string['diploma'] = 'CERTIFICATE';
+$string['linea1'] = 'This Diploma has been conferred for';
+$string['linea2'] = 'who has satisfactorily completed the course';
+
+?>
