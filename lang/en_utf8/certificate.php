6de38f59d7924ad6be745364f494c76b97c2763d
stronk7 <stronk7>
2006-06-20 22:17:57 +0000

    Updating to latest version of certificate module

---
+++ lang/en_utf8/certificate.php
@@ -0,0 +1,75 @@
+<?PHP
+
+$string['autopreview'] = 'Allows you to turn on/off preview of certiface while making changes.';
+
+$string['modulename'] = 'Certificate';
+$string['modulenameplural'] = 'Certificates';
+
+$string['certificate_type'] = 'Certificate Type';
+
+$string['border_style'] = 'Border Style';
+$string['border_none'] = 'No border';
+$string['border_lines'] = 'Lines';
+
+$string['border_color'] = 'Border Color';
+$string['border_black'] = 'Black';
+$string['border_brown'] = 'Brown';
+$string['border_blue'] = 'Blue';
+$string['border_green'] = 'Green';
+
+$string['print_wmark'] = 'Print Watermark';
+
+$string['date_format'] = 'Date Format';
+$string['no_date'] = 'Dont print date';
+
+$string['print_number'] = 'Print Cert Number';
+
+$string['print_grade'] = 'Print Grade';
+$string['dont_print_grade'] = 'Dont print grade';
+$string['course_grade'] = 'Course Grade';
+$string['no_grades'] = 'No grades available';
+
+$string['print_signature'] = 'Print Signature';
+$string['sig_line'] = 'line';
+
+$string['print_teacher'] = 'Print Teacher';
+
+$string['print_seal'] = 'Print Seal';
+$string['seal_no'] = 'No seal';
+
+// text for each certificate type
+$string['type0'] = 'Completion (H)';
+$string['title0'] = 'CERTIFICATE of COMPLETION';
+$string['intro0'] = 'This is to certify that';
+$string['statement0'] = 'has satisfactorily completed the course';
+
+$string['type1'] = 'Recognition (H)';
+$string['title1'] = 'IN RECOGNITON';
+$string['intro1'] = 'This is to certify that';
+$string['statement1'] = 'has completed';
+
+$string['type2'] = 'Achievement (H)';
+$string['title2'] = 'THIS CERTIFICATE of ACHIEVEMENT';
+$string['intro2'] = 'is awarded to';
+$string['statement2'] = 'in recognition of having successfully completed';
+
+$string['type3'] = 'Completion (V)';
+$string['title3'] = 'CERTIFICATE of COMPLETION';
+$string['intro3'] = 'This is to certify that';
+$string['hours3'] = 'has studied 10 contact hours';
+$string['statement3'] = 'and completed the advanced course';
+$string['onday3'] = 'on this day';
+
+//strings for verification block
+$string['configcontent'] = 'Config content';
+$string['validate'] = 'Verify';
+$string['certificate'] = 'Verification for certificate code:';
+$string['verifycertificate'] = 'Verify Certificate';
+$string['dontallowall'] = 'Do not allow all';
+$string['cert'] = '#';
+$string['notfound'] = 'The certificate number could not be validated.';
+$string['back'] = 'Back';
+$string['to'] = 'Awarded to';
+$string['course'] = 'For';
+$string['date'] = 'On';
+?>
