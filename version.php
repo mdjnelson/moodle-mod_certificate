2943a5288ca20b52aabd1e6e35cf559fabbe6fd2
crbusch <crbusch>
2007-07-01 00:49:50 +0000

    6/13/07

--- version.php
+++ version.php
@@ -1,11 +1,11 @@
-<?PHP // $Id: version.php,v 3.2 latest update 2006/10/12 
+<?PHP // $Id: version.php,v 3.1.8 
 
 ///////////////////////////////////////////////////////////////////////////////
 ///  Code fragment to define the version of certificate
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ///////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2007020900;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2007061300;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2006080900;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
