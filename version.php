ec1a58cb4f94e91aa605a6d9bc85553bff2e33f9
crbusch <crbusch>
2010-11-07 21:07:11 +0000

    new versioning

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ///////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2009062900;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010080300;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
