f4eb50c9000bd2d2c859c2277484344222419fe1
Mark Nelson <mark@moodle.com.au>
2012-01-06 00:40:36 +0800

    tidied up the code in review.php

--- review.php
+++ review.php
@@ -1,63 +1,78 @@
-<?PHP // $Id: version.php
+<?php
+
+/**
+ * This page reviews a certificate
+ *
+ * @package    mod
+ * @subpackage certificate
+ * @copyright Mark Nelson <mark@moodle.com.au>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once('../../config.php');
 require_once('lib.php');
-include '../../lib/pdflib.php';
+require_once("$CFG->libdir/pdflib.php");
 
-    $id = required_param('id', PARAM_INT);    // Course Module ID
-    $action = optional_param('action', '', PARAM_ALPHA);
+// Retreive any variables that are passed
+$id = required_param('id', PARAM_INT);    // Course Module ID
+$action = optional_param('action', '', PARAM_ALPHA);
 
-    if (! $cm = get_coursemodule_from_id('certificate', $id)) {
-        print_error('Course Module ID was incorrect');
-    }
-    if (! $course = $DB->get_record('course', array('id'=> $cm->course))) {
-        print_error('course is misconfigured');
-    }
-    if (! $certificate = $DB->get_record('certificate', array('id'=> $cm->instance))) {
-        print_error('course module is incorrect');
-    }
+if (!$cm = get_coursemodule_from_id('certificate', $id)) {
+    print_error('Course Module ID was incorrect');
+}
 
-    require_login($course->id, true, $cm);
-    $context = get_context_instance(CONTEXT_MODULE, $cm->id);
-    require_capability('mod/certificate:view', $context);
+if (!$course = $DB->get_record('course', array('id'=> $cm->course))) {
+    print_error('course is misconfigured');
+}
+
+if (!$certificate = $DB->get_record('certificate', array('id'=> $cm->instance))) {
+    print_error('course module is incorrect');
+}
+
+// Requires a course login
+require_course_login($course->id, true, $cm);
+
+// Check the capabilities
+$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+require_capability('mod/certificate:view', $context);
 
 // Initialize $PAGE, compute blocks
-    $PAGE->set_url('/mod/certificate/review.php', array('id' => $cm->id));
-    $PAGE->set_context($context);
-    $PAGE->set_cm($cm);
+$PAGE->set_url('/mod/certificate/review.php', array('id' => $cm->id));
+$PAGE->set_context($context);
+$PAGE->set_cm($cm);
 
-/// Get previous certrecord
-    $certificateid = $certificate->id;
-    $sql = 'SELECT MAX(timecreated) AS latest FROM {certificate_issues} '.
-           'WHERE userid = :userid and certificateid = :certificateid and certdate > 0';
-            if ($record = $DB->get_record_sql($sql, array('certificateid'=>$certificateid, 'userid'=>$USER->id))) {
-                $latest = $record->latest;
-            }
-    $certrecord = $DB->get_record('certificate_issues', array('certificateid'=>$certificateid, 'userid'=>$USER->id, 'timecreated'=>$latest));
+// Get previous certrecord
+$certificateid = $certificate->id;
+$sql = "SELECT MAX(timecreated) AS latest
+            FROM {certificate_issues}
+            WHERE userid = :userid
+            AND certificateid = :certificateid
+            AND certdate > 0";
+if ($record = $DB->get_record_sql($sql, array('certificateid'=>$certificateid, 'userid'=>$USER->id))) {
+    $latest = $record->latest;
+}
+$certrecord = $DB->get_record('certificate_issues', array('certificateid'=>$certificateid, 'userid'=>$USER->id, 'timecreated'=>$latest));
 
-/// Load custom type
-    $type = $certificate->certificatetype;
+// Load some strings
+$strreviewcertificate = get_string('reviewcertificate', 'certificate');
+$strgetcertificate = get_string('getcertificate', 'certificate');
+$strgrade = get_string('grade', 'certificate');
+$strcoursegrade = get_string('coursegrade', 'certificate');
+$strcredithours = get_string('credithours', 'certificate');
+$filename = clean_filename($certificate->name.'.pdf');
 
-/// Load some strings
-    $strreviewcertificate = get_string('reviewcertificate', 'certificate');
-    $strgetcertificate = get_string('getcertificate', 'certificate');
-    $strgrade = get_string('grade', 'certificate');
-    $strcoursegrade = get_string('coursegrade', 'certificate');
-    $strcredithours = get_string('credithours', 'certificate');
-    $filename = clean_filename($certificate->name.'.pdf');
+// Load the specific certificatetype
+require ("$CFG->dirroot/mod/certificate/type/$certificate->certificatetype/certificate.php");
 
-///Load the specific certificatetype
-    require ("$CFG->dirroot/mod/certificate/type/$certificate->certificatetype/certificate.php");
-
-        if(empty($action)) {
-            view_header($course, $certificate, $cm);
-        if ($certrecord->certdate > 0) { ///Review certificate
+if (empty($action)) {
+    view_header($course, $certificate, $cm);
+    if ($certrecord->certdate > 0) { // Review certificate
         if (empty($action)) {
             view_header($course, $certificate, $cm);
             $link = new moodle_url('/mod/certificate/review.php?id='.$cm->id.'&action=get');
             echo '<p align="center">'.get_string('viewed', 'certificate').'<br />'.userdate($certrecord->certdate).'</p>';
             echo '<center>';
-			$linkname = $strreviewcertificate;
+            $linkname = $strreviewcertificate;
             $button = new single_button($link, $linkname);
             $button->add_action(new popup_action('click', $link, array('height' => 600, 'width' => 800)));
             echo $OUTPUT->render($button);
@@ -68,4 +83,4 @@ include '../../lib/pdflib.php';
         }
     }
 }
-        $pdf->Output($filename, 'I');// open in browser
+$pdf->Output($filename, 'I'); // open in browser
