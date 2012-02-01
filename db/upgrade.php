4799798af2c93bd79b87266d16f9f125a382b122
Mark Nelson <mark@moodle.com.au>
2012-02-01 14:42:47 +0800

    CONTRIB-3370 - fixed use of deprecated functions in the upgrade script

--- db/upgrade.php
+++ db/upgrade.php
@@ -27,56 +27,56 @@ function xmldb_certificate_upgrade($oldversion=0) {
     //===== 1.9.0 or older upgrade line ======//
     if ($result && $oldversion < 2007102806) {
         // Add new fields to certificate table
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('printoutcome');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'gradefmt');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('printoutcome');
+        $field->set_attributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'gradefmt');
         $result = $result && add_field($table, $field);
     }
 
     if ($result && $oldversion < 2007102800) {
         // Add new fields to certificate table
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('reportcert');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'savecert');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('reportcert');
+        $field->set_attributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'savecert');
         $result = $result && add_field($table, $field);
 
-        $table = new XMLDBTable('certificate_issues');
-        $field = new XMLDBField('reportgrade');
-        $field->setAttributes(XMLDB_TYPE_CHAR, '10', null, null, null, null, null, null, 'certdate');
+        $table = new xmldb_table('certificate_issues');
+        $field = new xmldb_field('reportgrade');
+        $field->set_attributes(XMLDB_TYPE_CHAR, '10', null, null, null, null, 'certdate');
         $result = $result && add_field($table, $field);
     }
 
     if ($result && $oldversion < 2007061300) {
         // Add new fields to certificate table
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('emailothers');
-        $field->setAttributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, null, null, 'emailteachers');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('emailothers');
+        $field->set_attributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, 'emailteachers');
         $result = $result && add_field($table, $field);
 
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('printhours');
-        $field->setAttributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, null, null, 'gradefmt');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('printhours');
+        $field->set_attributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, 'gradefmt');
         $result = $result && add_field($table, $field);
 
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('lockgrade');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'printhours');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('lockgrade');
+        $field->set_attributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'printhours');
         $result = $result && add_field($table, $field);
 
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('requiredgrade');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'lockgrade');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('requiredgrade');
+        $field->set_attributes(XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'lockgrade');
         $result = $result && add_field($table, $field);
 
         // Rename field save to savecert
-        $field = new XMLDBField('save');
+        $field = new xmldb_field('save');
         if (field_exists($table, $field)) {
-            $field->setAttributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'emailothers');
+            $field->set_attributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'emailothers');
             // Launch rename field savecert
             $result = $result && rename_field($table, $field, 'savecert');
         } else {
-            $field = new XMLDBField('savecert');
-            $field->setAttributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'emailothers');
+            $field = new xmldb_field('savecert');
+            $field->set_attributes(XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'emailothers');
 
             $result = $result && add_field($table, $field);
         }
@@ -84,15 +84,15 @@ function xmldb_certificate_upgrade($oldversion=0) {
     }
 
     if ($result && $oldversion < 2007061301) {
-        $table = new XMLDBTable('certificate_linked_modules');
-        $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null, null);
-        $table->addFieldInfo('certificate_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'id');
-        $table->addFieldInfo('linkid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'certificate_id');
-        $table->addFieldInfo('linkgrade', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'linkid');
-        $table->addFieldInfo('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'linkgrade');
-        $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
-        $table->addIndexInfo('certificate_id', XMLDB_INDEX_NOTUNIQUE, array('certificate_id'));
-        $table->addIndexInfo('linkid', XMLDB_INDEX_NOTUNIQUE, array('linkid'));
+        $table = new xmldb_table('certificate_linked_modules');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE,  null, null);
+        $table->add_field('certificate_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'id');
+        $table->add_field('linkid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'certificate_id');
+        $table->add_field('linkgrade', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'linkid');
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'linkgrade');
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'), null, null);
+        $table->add_index('certificate_id', XMLDB_INDEX_NOTUNIQUE, array('certificate_id'));
+        $table->add_index('linkid', XMLDB_INDEX_NOTUNIQUE, array('linkid'));
         $result = create_table($table);
 
         if ($result) {
@@ -102,18 +102,18 @@ function xmldb_certificate_upgrade($oldversion=0) {
     }
 
     if ($result && $oldversion < 2007061302) {
-        $table  = new XMLDBTable('certificate_linked_modules');
-        $field = new XMLDBField('linkid');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, null, '0', 'certificate_id');
+        $table  = new xmldb_table('certificate_linked_modules');
+        $field = new xmldb_field('linkid');
+        $field->set_attributes(XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'certificate_id');
         $result = change_field_unsigned($table, $field);
     }
 
     if ($result && $oldversion < 2008080904) {
         // Add new fields to certificate table if they dont already exist
 
-        $table = new XMLDBTable('certificate');
-        $field = new XMLDBField('intro');
-        $field->setAttributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, null, null, 'name');
+        $table = new xmldb_table('certificate');
+        $field = new xmldb_field('intro');
+        $field->set_attributes(XMLDB_TYPE_TEXT, 'small', null, null, null, null, 'name');
         if (!field_exists($table, $field)) {
             $result = $result && add_field($table, $field);
         }
