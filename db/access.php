e8c7a47ea07a71a241f6ffb5093254032351c925
Mark Nelson <mark@moodle.com.au>
2012-05-08 23:28:07 +0800

    added new capability required for 2.3, and updated version.php to trigger an update

--- db/access.php
+++ db/access.php
@@ -33,6 +33,18 @@
 
 $capabilities = array(
 
+     'mod/certificate:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+
     'mod/certificate:view' => array(
 
         'captype' => 'read',
@@ -55,7 +67,7 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-    
+
      'mod/certificate:printteacher' => array(
 
         'captype' => 'read',
@@ -66,5 +78,5 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-    
+
 );
