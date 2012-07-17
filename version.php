f82b1f413ef43118a73ef43428975c1394a9572a
Mark Nelson <mark@moodle.com.au>
2012-07-17 17:13:35 +0800

    CONTRIB-3709 - Added missing attributes to the version.php file

--- version.php
+++ version.php
@@ -9,6 +9,10 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  **/
 
-$module->version  = 2012070201;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010112400;  // Requires this Moodle version
-$module->cron     = 0;           // Period for cron to check this module (secs)
+$module->version    = 2012070201;  // The current module version (Date: YYYYMMDDXX)
+$module->requires   = 2012062500;  // Requires this Moodle version
+$module->cron       = 0;           // Period for cron to check this module (secs)
+$module->component  = 'mod_certificate';
+
+$module->maturity   = MATURITY_STABLE;
+$module->release    = "2.3 (2012070201)"; // User-friendly version number
