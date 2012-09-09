eec11c554475bb47cec22c437f2984f24ebc6f62
Mark Nelson <mark@moodle.com.au>
2012-09-09 18:23:41 +0800

    Increasing version number

--- version.php
+++ version.php
@@ -9,10 +9,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version   = 2012082402; // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2012090902; // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2012062500; // Requires this Moodle version
 $module->cron      = 0; // Period for cron to check this module (secs)
 $module->component = 'mod_certificate';
 
 $module->maturity  = MATURITY_STABLE;
-$module->release   = "2.3 (2012082402)"; // User-friendly version number
+$module->release   = "2.3 (2012090902)"; // User-friendly version number
