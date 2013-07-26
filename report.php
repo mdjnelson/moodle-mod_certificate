685c513f1cf25539bd2ba23f9d2d70ebb41e5a0c
Mark Nelson <markn@moodle.com>
2013-07-26 12:24:02 +0800

    Removed usages of deprecated context functions

--- report.php
+++ report.php
@@ -70,7 +70,7 @@ if (!$certificate = $DB->get_record('certificate', array('id'=> $cm->instance)))
 require_course_login($course->id, false, $cm);
 
 // Check capabilities
-$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+$context = context_module::instance($cm->id);
 require_capability('mod/certificate:manage', $context);
 
 // Declare some variables
