3ee3a4f166992de50c47c35593d912bdb17a2f6f
Mark Nelson <markn@moodle.com>
2014-08-19 14:50:18 -0700

    Version bump

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$plugin->version   = 2014041800; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014041801; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013111800; // Requires this Moodle version
 $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_certificate';
