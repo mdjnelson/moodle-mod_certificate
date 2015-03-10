c47ac61a2194f42b79ef581e35c64c592b27af1b
Mark Nelson <markn@moodle.com>
2015-03-10 01:12:52 -0700

    Fixed incorrect package in PHPDocs

--- db/log.php
+++ db/log.php
@@ -18,7 +18,7 @@
 /**
  * Definition of log events
  *
- * @package    mod_lesson
+ * @package    mod_certificate
  * @copyright  2010 Petr Skoda (http://skodak.org)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
