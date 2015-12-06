c1d0742d8550a09643ec0f69d482c5001574dd30
Mark Nelson <markn@moodle.com>
2015-12-06 15:26:06 +0800

    CONTRIB-5955: Made release string more user friendly

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015083100; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2015083101; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100; // Requires this Moodle version
 $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_certificate';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Stable"; // User-friendly version number
+$plugin->release   = "2.9 (Build: 2015083101)"; // User-friendly version number
