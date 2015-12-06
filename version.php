826457d16f60c1e49125b438c4054ead7bb1a83f
Mark Nelson <markn@moodle.com>
2015-12-06 15:19:51 +0800

    Increased version number and the version required

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015083100; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015051100; // Requires this Moodle version
+$plugin->version   = 2015120600; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015111600; // Requires this Moodle version
 $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_certificate';
 
