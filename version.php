08a0b04ab3884267ca387e97ef254659923be30f
Mark Nelson <markn@moodle.com>
2016-06-17 10:46:16 +0800

    Bumped version and version required

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120602; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015111600; // Requires this Moodle version
+$plugin->version   = 2016052300; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2016052300; // Requires this Moodle version (3.1)
 $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_certificate';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Master (Build: 2015120601)"; // User-friendly version number
+$plugin->release   = "3.1 (Build: 2016052300)"; // User-friendly version number
